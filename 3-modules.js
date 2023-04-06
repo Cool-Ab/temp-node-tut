8b3c0e6b37f3d9b44a511c15804992e33d77c10f
abel <abelemek@gmail.com>
2023-04-06 12:10:42 +0300

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,9 @@
+// modules
+
+const names=require('./4-names')
+const utils=require('./5-utils')
+const data=require('./6-alternative-flavour')
+require('./7-mind-gerned')
+//utils('susan')
+//utils(names.john)
+//utils(names.john)
